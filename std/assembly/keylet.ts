0b9f197bdd76956f15a5de7c3263fb664fe7aaf4
Vaclav Barta <vbarta@mangrove.cz>
2022-12-12 10:16:28 +0100

    fixed check of util_keylet result

--- std/assembly/keylet.ts
+++ std/assembly/keylet.ts
@@ -6,7 +6,7 @@ export class Keylet {
     let a = new ByteArray(34);
     let b = acc.bytes;
     let r = $util_keylet(changetype<u32>(a), 34, KEYLET_SIGNERS, changetype<u32>(b), 20, 0, 0, 0, 0);
-    if (r != 20)
+    if (r != 34)
       rollback("", r);
 
     return a;
