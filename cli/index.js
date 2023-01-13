e6d241547a33e557d1db1822c3c7f092b65ee0b2
muzam1l <muzamilsofi@outlook.com>
2023-01-13 13:47:40 +0530

    Use dist asc in cli instead of lkg.

--- cli/index.js
+++ cli/index.js
@@ -34,7 +34,7 @@ import * as optionsUtil from "../util/options.js";
 import * as generated from "./index.generated.js";
 
 import binaryen from "../lib/binaryen.js";
-import * as assemblyscriptJS from "../lib/lkg/assemblyscript";
+import * as assemblyscriptJS from "../dist/assemblyscript.js";
 
 // Use the TS->JS variant by default
 let assemblyscript = assemblyscriptJS;
