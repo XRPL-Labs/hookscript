XRPL-Labs/hookscript

commit 565d2c591d4b6e744c02b3de7b31aab0386e1718

added HookParam<Account>

std/assembly/account.ts
@@ -4,9 +4,11 @@
 import { accept } from "./bindings/hookapi";
 
 export class Account {
+  static readonly dataSize: u32 = 20;
+
   @inline
   constructor(public bytes: ByteArray) {
-    if (bytes.length != 20)
+    if (bytes.length != Account.dataSize)
       rollback("", pack_error_code(bytes.length));
   }
 
@@ -15,7 +17,7 @@ export class Account {
     let ptr2 = changetype<usize>(right);
     if (ptr1 == ptr2) return 0;
 
-    for (let i = 0; max_iterations(20), i < 20; ++i) {
+    for (let i = 0; max_iterations(20), i < Account.dataSize; ++i) {
       let a = load<u8>(ptr1);
       let b = load<u8>(ptr2);
       if (a > b)

std/assembly/params.ts
@@ -1,3 +1,27 @@
+export class HookParamSpec {
+  name: string;
+  description: string | null;
+}
+
+export class HookParam<T> {
+  name: string;
+
+  @inline
+  constructor(ps: HookParamSpec) {
+    this.name = ps.name
+  }
+
+  @inline
+  get(): T {
+    let a = new ByteArray(data_size<T>());
+    let r = $hook_param(changetype<u32>(a), data_size<T>(), changetype<u32>(this.name), this.name.length);
+    if (r != data_size<T>())
+      rollback("", pack_error_code(r));
+
+    return instantiate<T>(a);
+  }
+};
+
 export namespace Params {
   // compiler re-directs all names inside this namespace here
   @inline

commit 6888c148ab3d7466bfab2c6678a2e92949dbebc4

fixed regression (uncompilable code) caused by 565d2c591d4b6e744c02b3de7b31aab0386e1718

std/assembly/account.ts
@@ -19,7 +19,7 @@ export class Account {
     let ptr2 = changetype<usize>(right);
     if (ptr1 == ptr2) return 0;
 
-    for (let i = 0; max_iterations(20), i < Account.dataSize; ++i) {
+    for (let i: u32 = 0; max_iterations(20), i < Account.dataSize; ++i) {
       let a = load<u8>(ptr1);
       let b = load<u8>(ptr2);
       if (a > b)