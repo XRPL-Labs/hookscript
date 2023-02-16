d2dbf924c5de525372785b8d3beb628fad508cdb
Vaclav Barta <vbarta@mangrove.cz>
2023-02-16 15:52:33 +0100

    added min incoming rule

--- hook-api-examples/firewall/firewall.ts
+++ hook-api-examples/firewall/firewall.ts
@@ -21,9 +21,14 @@ function hook(reserved: i32)
 
     const hook_accid = hook_account();
     const account_field = Tx.Account;
-    const side_name = (account_field == hook_accid) ? 'outgoing' : 'incoming';
+    const outgoing_flag = account_field == hook_accid
+    const side_name = outgoing_flag ? 'outgoing' : 'incoming'
     const side = new HookParam<ByteSet>({
         name: side_name
+    })
+
+    const threshold = new HookParam<u32>({
+        name: "min_incoming"
     })
 
     const allow_all = new ByteArray(32)
@@ -31,12 +36,23 @@ function hook(reserved: i32)
         max_iterations(32)
         allow_all[i] = 0
     }
-  
+
     const filter = side.get(new ByteSet(allow_all))
-  
+
+    let min_incoming = 0
+    if (!outgoing_flag)
+        min_incoming = threshold.value(0)
+
     const transaction_type = <u8>Tx.TransactionType
     if (filter.has(transaction_type))
         rollback(`Firewall: Blocking ${side_name} transaction of type ${transaction_type}`)
+
+    const amount = Tx.Amount
+    if (amount.isXrp()) {
+        let otxn_drops: i64 = amount.drops
+        if ((min_incoming > 0) && (otxn_drops < min_incoming))
+            rollback(`Firewall: Blocking ${<u32>otxn_drops} drops as too small`)
+    }
 
     accept("Firewall: Allowing transaction.")
 }
