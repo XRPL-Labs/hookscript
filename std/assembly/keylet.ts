86c65f3b69f2ba6293b318f4ed17d732971229fe
Vaclav Barta <vbarta@mangrove.cz>
2023-03-01 14:59:23 +0100

    added wrapper for util_keylet(...KEYLET_HOOK_STATE...) (#78)

--- std/assembly/keylet.ts
+++ std/assembly/keylet.ts
@@ -1,5 +1,9 @@
 // @ts-ignore: decorator
 @lazy
+export const KEYLET_HOOK_STATE = 2;
+
+// @ts-ignore: decorator
+@lazy
 export const KEYLET_LINE = 9;
 
 // @ts-ignore: decorator
@@ -7,6 +11,22 @@ export const KEYLET_LINE = 9;
 export const KEYLET_SIGNERS = 14;
 
 export class Keylet {
+  @inline
+  static getHookState(acc: Account, key: ByteArray, ns: ByteArray): ByteArray {
+    let a = new ByteArray(34);
+    let b = acc.bytes;
+    let r = $util_keylet(
+      changetype<u32>(a), 34,
+      KEYLET_HOOK_STATE,
+      changetype<u32>(b), 20,
+      changetype<u32>(key), key.length,
+      changetype<u32>(ns), ns.length);
+    if (r != 34)
+      rollback("", pack_error_code(r));
+
+    return a;
+  }
+
   @inline
   static getSigners(acc: Account): ByteArray {
     let a = new ByteArray(34);
